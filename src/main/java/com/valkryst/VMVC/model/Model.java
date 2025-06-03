a4d3c37d68716f5ad7e6f17ffd1355b2e8869303
Valkryst <github@valkryst.com>
2025-06-03 14:54:59 -0300

    Update `Model` to use `ViewInterface`

--- src/main/java/com/valkryst/VMVC/model/Model.java
+++ src/main/java/com/valkryst/VMVC/model/Model.java
@@ -2,6 +2,7 @@ package com.valkryst.VMVC.model;
 
 import com.valkryst.VMVC.controller.Controller;
 import com.valkryst.VMVC.view.View;
+import com.valkryst.VMVC.view.ViewInterface;
 
 /**
  * A set of data that can be displayed by a {@link View}.
@@ -9,7 +10,7 @@ import com.valkryst.VMVC.view.View;
  * @param <C> The type of {@link Controller} associated with this {@code Model}.
  * @param <V> The type of {@link View} associated with this {@code Model}.
  */
-public abstract class Model<C extends Controller<?>, V extends View<?>> {
+public abstract class Model<C extends Controller<?>, V extends ViewInterface> {
     /**
      * Constructs a {@link Controller} for the {@link View}.
      *
